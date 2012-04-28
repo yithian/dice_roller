43fa76af891046eee3912d1eac08778eae1a410d
Alex Chvatal <m.chvatal@gmail.com>
2012-04-27 21:27:19 -0400

    test dice results

--- test/test_dice_result.rb
+++ test/test_dice_result.rb
@@ -3,12 +3,45 @@ require 'dice_roller'
 
 class DicePoolTest < Test::Unit::TestCase
   def setup
-    @dr = DiceRoller::DiceResult.new(1, 1, 1, 1, 1, 1, 1)
+    @dr = DiceRoller::DiceResult.new([1], [1], [1], [1, 4, 9, 10], [1], [1], [1])
   end
 
-  def test_sum
+  def test_total
+    assert_equal @dr.total, 30, "sum didn't calculate correctly"
   end
 
-  def test_successes
+  def test_successes_normal
+    suxx = @dr.successes
+    
+    assert suxx >= 2, "should have rolled more successes"
+    assert @dr.ten_result.count >= 5, "didn't reroll a 10"
+  end
+  
+  def test_successes_minimum
+    suxx = @dr.successes(10)
+    
+    assert suxx >= 1, "should have rolled more successes"
+    assert @dr.ten_result.count >= 5, "didn't reroll a 10"
+  end
+  
+  def test_successes_reroll
+    suxx = @dr.successes(8, 9)
+    
+    assert suxx >= 2, "should have rolled more successes"
+    assert @dr.ten_result.count >= 6, "didn't reroll a 9 or 10"
+  end
+  
+  def test_successes_subtract
+    suxx = @dr.successes(8, 10, true)
+    
+    assert suxx >= 0, "should have rolled more successes"
+    assert @dr.ten_result.count >= 5, "didn't reroll a 10"
+  end
+  
+  def test_successes_rote
+    suxx = @dr.successes(8, 0)
+    
+    assert suxx >= 2, "should have rolled more successes"
+    assert @dr.ten_result.count >= 7, "didn't reroll per rote action"
   end
 end
